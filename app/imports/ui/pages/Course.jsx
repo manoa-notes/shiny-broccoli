80b9dd31543b39a8c24222c6652f9d67080c0549
aj-patalinghog <99686240+aj-patalinghog@users.noreply.github.com>
2022-11-29 20:04:28 -1000

    Updated list course page to reflect number of notes.

--- app/imports/ui/pages/Course.jsx
+++ app/imports/ui/pages/Course.jsx
@@ -2,16 +2,17 @@ import React from 'react';
 import { useParams } from 'react-router';
 import { useTracker } from 'meteor/react-meteor-data';
 import { Meteor } from 'meteor/meteor';
-import { Container, Row } from 'react-bootstrap';
+import { Button, Container, Row } from 'react-bootstrap';
 import { Courses } from '../../api/course/Courses';
 import LoadingSpinner from '../components/LoadingSpinner';
 import { Notes } from '../../api/note/Note';
 import NoteCard from '../components/NoteCard';
+import { Link } from 'react-router-dom';
 
 const Course = () => {
   const { path } = useParams();
   // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
-  const { ready, course } = useTracker(() => {
+  const { ready, course, notes } = useTracker(() => {
     // Note that this subscription will get cleaned up
     // when your component is unmounted or deps change.
     // Get access to Stuff documents.
@@ -23,21 +24,38 @@ const Course = () => {
     const rdy = rdy1 && rdy2;
     // Get the Stuff documents
     const courseItem = Courses.collection.findOne({ path: path });
+    const noteItems = Notes.collection.find({ course: courseItem.name }).fetch();
     return {
       course: courseItem,
+      notes: noteItems,
       ready: rdy,
     };
   }, []);
+
+  if (notes.length === 0) {
+    return ready ? (
+      <Container className="py-3">
+        <h1>{course.name}</h1>
+        <h2>Notes</h2>
+        <Row>
+          <p style={{ fontSize: '18px' }}>
+            There are currently no notes for this class. Add some here:
+            <Button className="ms-2" variant="success" as={Link} to="/addNote">Add notes</Button>
+          </p>
+        </Row>
+      </Container>
+    ) : <LoadingSpinner />;
+  }
   return ready ? (
     <Container className="py-3">
       <h1>{course.name}</h1>
       <h2>Notes</h2>
       <Row className="py-2">
-        {Notes.collection.find({ course: course.name }).fetch().map(note => <NoteCard note={note} />)}
+        {notes.map(note => <NoteCard note={note} />)}
       </Row>
-      <h2>Forum Posts</h2>
     </Container>
   ) : <LoadingSpinner />;
+
 };
 
 export default Course;
--- app/imports/ui/pages/ListCourses.jsx
+++ app/imports/ui/pages/ListCourses.jsx
@@ -5,6 +5,7 @@ import { useTracker } from 'meteor/react-meteor-data';
 import { Meteor } from 'meteor/meteor';
 import LoadingSpinner from '../components/LoadingSpinner';
 import { Courses } from '../../api/course/Courses';
+import { Notes } from '../../api/note/Note';
 
 const ListCourses = () => {
   // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
@@ -12,9 +13,12 @@ const ListCourses = () => {
     // Note that this subscription will get cleaned up
     // when your component is unmounted or deps change.
     // Get access to Stuff documents.
-    const subscription = Meteor.subscribe(Courses.userPublicationName);
+    const sub1 = Meteor.subscribe(Courses.userPublicationName);
+    const sub2 = Meteor.subscribe(Notes.userPublicationName);
     // Determine if the subscription is ready
-    const rdy = subscription.ready();
+    const rdy1 = sub1.ready();
+    const rdy2 = sub2.ready();
+    const rdy = rdy1 && rdy2;
     // Get the Stuff documents
     const courseItems = Courses.collection.find({}, { sort: { name: 1 } }).fetch();
     return {
@@ -28,7 +32,9 @@ const ListCourses = () => {
       <Row>
         {courses.map(course => (
           <Col md={3} className="d-grid py-2" key={course._id}>
-            <Button variant="success" size="lg" as={Link} to={`/courses/${course.path}`}>{course.name}</Button>
+            <Button variant="success" size="lg" as={Link} to={`/courses/${course.path}`}>
+              {course.name} ({Notes.collection.find({ course: course.name }).fetch().length} notes)
+            </Button>
           </Col>
         ))}
       </Row>
