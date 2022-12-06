67109f6e89b114f8e77bc92198c98338fe06bb34
lindanguyen25 <linda68@hawaii.edu>
2022-12-06 11:44:40 -1000

    search bar

--- app/imports/ui/pages/ListNotes.jsx
+++ app/imports/ui/pages/ListNotes.jsx
@@ -1,31 +1,48 @@
-import React from 'react';
-import { Meteor } from 'meteor/meteor';
-import { Button, Col, Container, Row } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { Button, Col, Container, Nav, Row } from 'react-bootstrap';
 import { useTracker } from 'meteor/react-meteor-data';
 import { Link } from 'react-router-dom';
 import { Notes } from '../../api/note/Note';
-import LoadingSpinner from '../components/LoadingSpinner';
 import NoteCard from '../components/NoteCard';
 import { ComponentIDs, PageIDs } from '../utilities/ids';
+import Searches from '../components/Searches';
 
+const ALL = 'All';
 /* Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 const ListNotes = () => {
-  const { ready, notes } = useTracker(() => {
-    // Note that this subscription will get cleaned up
-    // when your component is unmounted or deps change.
-    // Get access to Stuff documents.
-    const sub = Meteor.subscribe(Notes.userPublicationName);
-    // Determine if the subscription is ready
-    const rdy = sub.ready();
-    // Get the Stuff documents
+  // eslint-disable-next-line no-unused-vars
+  const [showItems, setShowItems] = useState([]);
+  const [title, setTitle] = useState(ALL);
+  const [search, setSearch] = useState('');
+
+  const handleSearch = (input) => { setSearch(`${input}`); };
+  const handleCategoryType = (category) => {
+    handleSearch('');
+    setTitle(`${category}`);
+  };
+
+  const { notes } = useTracker(() => {
     const notesItems = Notes.collection.find({}).fetch();
     return {
       notes: notesItems,
-      ready: rdy,
     };
   }, []);
+  useEffect(() => {
+    console.log('rendered');
+    document.title = 'ManoaXchange - Shop';
+    if (search.length > 0) {
+      setShowItems(notes.filter(note => note.title.toLowerCase().includes(search)));
+      setTitle(ALL);
+    } else if (title === ALL) {
+      setShowItems(notes);
+    } else {
+      setShowItems(notes.filter(note => note.course === title));
+    }
+  }, [notes.length, title, search]);
 
-  return (ready ? (
+  console.log('title:', title);
+  console.log('search:', search);
+  return (
     <Container className="py-3" id={PageIDs.listNotesPage}>
       <Row className="align-items-center">
         <Col md={2}>
@@ -36,10 +53,29 @@ const ListNotes = () => {
         </Col>
       </Row>
       <Row>
+        <Nav className="d-flex flex-column">
+          <Nav.Item key="searchbox">
+            <Searches handleSearch={handleSearch} />
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link
+              onClick={() => handleCategoryType('All')}
+              className="text-light d-flex gap-2 align-items-center"
+              style={{ paddingLeft: 0 }}
+            />
+          </Nav.Item>
+        </Nav>
+      </Row>
+      <Row>
+        {showItems.length > 0
+            ? showItems.map(note => <NoteCard key={note._id} note={note} removable={false} />)
+            : <div> No items found </div>}
+      </Row>
+      <Row>
         {notes.map(note => <NoteCard key={note._id} note={note} removable={false} />)}
       </Row>
     </Container>
-  ) : <LoadingSpinner />);
+  );
 };
 
 export default ListNotes;
